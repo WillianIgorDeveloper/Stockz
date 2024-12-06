18cb529e76cc04d39b5d01f4eeecae361793cbba
Willian Igor <willanigordeveloper@gmail.com>
2024-12-06 15:54:19 -0300

    Feat: adicionando biome.js e react-router-doom

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import "./index.css";
+import { ROUTES } from "@utils"
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import "./index.css"
+import { IndexPage } from "@pages/index"
 
-createRoot(document.getElementById("root")!).render(<StrictMode />);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path={ROUTES.INDEX} element={<IndexPage />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>,
+)
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,29 @@
-import { defineConfig } from "vite";
+import path from "node:path";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+			"@entities": path.resolve(__dirname, "./src/models/entities"),
+			"@errors": path.resolve(__dirname, "./src/models/errors"),
+			"@factories": path.resolve(__dirname, "./src/models/factories"),
+			"@interfaces": path.resolve(__dirname, "./src/models/interfaces"),
+			"@providers": path.resolve(__dirname, "./src/models/providers"),
+			"@usecases": path.resolve(__dirname, "./src/models/usecases"),
+			"@composed": path.resolve(
+				__dirname,
+				"./src/presenters/components/composed",
+			),
+			"@ui": path.resolve(__dirname, "./src/presenters/components/ui"),
+			"@contexts": path.resolve(__dirname, "./src/presenters/contexts"),
+			"@hooks": path.resolve(__dirname, "./src/presenters/hooks"),
+			"@layouts": path.resolve(__dirname, "./src/presenters/layouts"),
+			"@middlewares": path.resolve(__dirname, "./src/presenters/middlewares"),
+			"@pages": path.resolve(__dirname, "./src/presenters/pages"),
+			"@utils": path.resolve(__dirname, "./src/utils"),
+		},
+	},
 });
